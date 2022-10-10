c641904bed851d1906b51797840aeb1bb46eccbe
Pixeldeck <philipmir@hotmail.com>
2022-10-10 16:07:25 +0200

    The MAP program, 1.1

---
+++ Telefonlista/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Map<String, String> Telefonlista = new HashMap<>();
+
+
+        Telefonlista.put("Greta", "543 3431 1533");
+        Telefonlista.put("Karin", "634 2341 2344");
+        Telefonlista.put("Kevin", "324 5430 1222");
+
+        System.out.println(Telefonlista.get("Greta"));
+
+        System.out.println("Vilket namn");
+        String val = sc.nextLine();
+
+
+
+
+        for(String nummer : Telefonlista.keySet()) {
+             //  System.out.println(nummer);
+               if (nummer.equals(val)) {
+                   System.out.println("Nummer: " + Telefonlista.get(nummer));
+                   System.out.println("Namn: " + nummer);
+               }
+           }
+
+    }
+
+
+}
