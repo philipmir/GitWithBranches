2ce2d95c55bf512f4ea9d0cc847f4626561e849a
Pixeldeck <philipmir@hotmail.com>
2022-10-10 18:47:48 +0200

    Lexicon delen

---
+++ Lexicon/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Map<String, String> Lexicon = new HashMap<>();
+
+        Lexicon.put("hej", "hello");
+        Lexicon.put("middag", "dinner");
+        Lexicon.put("fantasktisk", "fantastic");
+
+        
+
+
+
+    }
+}
